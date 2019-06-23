aa2836aaf0fdf896b83081a4ca43d25e3a77cfb2
iwannatto <iwannatto@gmail.com>
2019-06-23 09:14:08 +0000

    Fix contoller to allow date

--- app/controllers/buyings_controller.rb
+++ app/controllers/buyings_controller.rb
@@ -69,6 +69,6 @@ class BuyingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def buying_params
-      params.require(:buying).permit(:name, :price)
+      params.require(:buying).permit(:name, :price, :date)
     end
 end
